34139f5f85122ec85c7412b702337c702afd8683
devchoplife <dritedurojaiye@gmail.com>
2021-05-13 21:17:52 +0100

    Wrote the json Handleer

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -15,6 +16,33 @@ func defaultMux() *http.ServeMux {
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, World!")
+}
+
+func MapHandler(pathsToURLs map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if path, ok := pathsToURLs[r.URL.Path]; ok {
+			http.Redirect(w, r, path, http.StatusFound)
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+func JSONHandler(jsonData []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var pathsToURLs []struct {
+		Path string `json:"path"`
+		URL  string `json:"url"`
+	}
+	if err := json.Unmarshal(jsonData, &pathsToURLs); err != nil {
+		return nil, err
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		for _, urlPath := range pathsToURLs {
+			if urlPath.Path == r.URL.Path {
+				http.Redirect(w, r, urlPath, http.StatusFound)
+			}
+		}
+		fallback.ServeHTTP(w, r)
+	}, nil
 }
 
 func main() {
@@ -37,5 +65,17 @@ func main() {
 
 	if jsonFile != "" {
 		jsonData, err := ioutil.ReadFile(jsonFile)
+		if err != nil {
+			panic(err)
+		}
+
+		//Build the JSONHandler using the mapHandler as the fallback
+		jsonHandler, err := JSONHandler([]byte(jsonData), mapHandler)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Starting the server on:8080")
+		http.ListenAndServe(":8080", jsonHandler)
+	} else if yamlFile != "" {
+		
 	}
-}
